6d42d0687adfdebe390b7a93f7d3a5684e4264e4
Huynh Minh Tu <huynhminhtufu@gmail.com>
2019-09-02 17:40:37 +0700

    I am Root. We are Root.

--- models/confession.go
+++ models/confession.go
@@ -7,7 +7,7 @@ type Confession struct {
 	Content   string    `json:"content" db:"content"`
 	Sender    string    `json:"sender" db:"sender"`
 	Status    int       `json:"status" db:"status"`
-	Approver  string    `json:"approver" db:"approver"`
+	Validator string    `json:"validator" db:"validator"`
 	Reason    string    `json:"reason" db:"reason"`
 	CreatedAt time.Time `json:"created_at" db:"created_at"`
 	UpdatedAt time.Time `json:"updated_at" db:"updated_at"`
