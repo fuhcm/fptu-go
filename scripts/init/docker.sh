dc8ec07e9f64e9ff6fbef73c9566e1c496bd0120
Huynh Minh Tu <huynhminhtufu@gmail.com>
2019-09-02 18:16:46 +0700

    I CAN HAZ PYTHON, I CAN HAZ INDENTS

---
+++ scripts/init/docker.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+
+echo "Installing Docker and Docker Compose"
+sudo apt install apt-transport-https ca-certificates curl software-properties-common -y
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu bionic stable"
+sudo apt update
+apt-cache policy docker-ce
+sudo apt install docker-ce -y
+sudo curl -L "https://github.com/docker/compose/releases/download/1.24.1/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+sudo chmod +x /usr/local/bin/docker-compose
